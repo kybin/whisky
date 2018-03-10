b918c9230e68b29b7e5cc77924b41638d29224b8
kim yongbin <kybinz@gmail.com>
2018-03-11 02:42:29 +0900

    use bolt db instead of files

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"errors"
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
 	"regexp"
 	"text/template"
+	"time"
+
+	"github.com/boltdb/bolt"
 
 	blackfriday "v/gopkg.in/russross/blackfriday.v2@v1.0.0-gopkgin-v2.0.0"
 )
+
+var db *bolt.DB
 
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
@@ -21,8 +26,11 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+	return db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("body"))
+		err := b.Put([]byte(p.Title), p.Body)
+		return err
+	})
 }
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
@@ -37,16 +45,21 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
+	var body []byte
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("body"))
+		body = b.Get([]byte(title))
+		return nil
+	})
+	if body == nil {
+		return nil, errors.New("page not exists")
 	}
-	return &Page{Title: title, Body: blackfriday.Run(body)}, nil
+	return &Page{Title: title, Body: body}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
+	p.Body = blackfriday.Run(p.Body)
 	if err != nil {
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
@@ -89,8 +102,24 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 }
 
 func main() {
+	var err error
+	db, err = bolt.Open("whisky.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("body"))
+		if err != nil {
+			return fmt.Errorf("create buckets: %s", err)
+		}
+		return nil
+	})
+
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
